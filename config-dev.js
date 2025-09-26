717270eec66dc775b0a8a676b17e98d19b5030c8
Ubirajara Filho <110053911+ucgfilho@users.noreply.github.com>
2025-09-26 16:48:13 -0300

    feat: melhora configuração dos ambientes e documentação do projeto

--- config-dev.js
+++ config-dev.js
@@ -1,24 +1,22 @@
-const { defineConfig } = require('cypress')
-const baseConfig = require('./cypress.config')
-const dotenv = require('dotenv')
-const path = require('path')
+const { defineConfig } = require("cypress");
+const baseConfig = require("./cypress.config");
+const dotenv = require("dotenv");
+const path = require("path");
 
-dotenv.config(
-    {
-        path: path.resolve(__dirname, '.env.dev')
-    }
-)
+dotenv.config({
+  path: path.resolve(__dirname, ".env.dev"),
+});
 
 const e2e = {
-    baseUrl: process.env.BASE_URL,
-    env: {
-        username: process.env.USER,
-        password: process.env.PASSWORD
-    },
-    setupNodeEvents: baseConfig.e2e.setupNodeEvents
-}
+  baseUrl: process.env.BASE_URL,
+  env: {
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+  },
+  setupNodeEvents: baseConfig.e2e.setupNodeEvents,
+};
 
 module.exports = defineConfig({
-    ...baseConfig,
-    e2e
-})
+  ...baseConfig,
+  e2e,
+});
--- config-qa.js
+++ config-qa.js
@@ -1,16 +1,16 @@
-const { defineConfig } = require('cypress')
-const baseConfig = require('./cypress.config')
+const { defineConfig } = require("cypress");
+const baseConfig = require("./cypress.config");
 
 const e2e = {
-    baseUrl: 'https://www.qa.saucedemo.com',
-    env: {
-        username: 'QA_sauce',
-        password: 'QA_secret'
-    },
-    setupNodeEvents: baseConfig.e2e.setupNodeEvents
-}
+  baseUrl: "https://www.qa.saucedemo.com",
+  env: {
+    username: "QA_sauce",
+    password: "QA_secret",
+  },
+  setupNodeEvents: baseConfig.e2e.setupNodeEvents,
+};
 
 module.exports = defineConfig({
-    ...baseConfig,
-    e2e
-})
+  ...baseConfig,
+  e2e,
+});
